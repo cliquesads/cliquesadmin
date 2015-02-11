0040e4a7f71d52cb8b5da2e942bbf79e424f0550
Ben Liang <bliang@cliquesads.com>
2015-02-11 11:46:31 -0500

    NF: Function to get all disk names

--- cliquesadmin/gce_utils/snapshots.py
+++ cliquesadmin/gce_utils/snapshots.py
@@ -1,15 +1,20 @@
-__author__ = 'bliang'
-from cliquesadmin.gce_utils import *
+import sys
+from cliquesadmin.gce_utils import CliquesGCESettings, authenticate_and_build
 
-
-def make_snapshot(gce_service, auth_http):
-
-    # List instances
-    request = gce_service.disks().list(project=PROJECT_ID, filter=None, zone=DEFAULT_ZONE)
+def get_all_disks(auth_http, gce_service, gce_settings=CliquesGCESettings):
+    """
+    Gets a list of all disk names for a project
+    """
+    # List disks
+    request = gce_service.disks().list(project=gce_settings.PROJECT_ID,
+                                       filter=None,
+                                       zone=gce_settings.ZONE)
     response = request.execute(http=auth_http)
     if response and 'items' in response:
-        instances = response['items']
-        for instance in instances:
-            print instance['name']
-    else:
-        print 'No instances to list.'
+        all_disks = [o['name'] for o in response['items']]
+        return all_disks
+
+if __name__ == "__main__":
+    auth_http, gce_service = authenticate_and_build(sys.argv)
+    k = get_all_disks(auth_http, gce_service)
+    print k
