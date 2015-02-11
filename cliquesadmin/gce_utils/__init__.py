ae863f1af3335bbbd0aae936dd3c4a821c595161
Ben Liang <bliang@cliquesads.com>
2015-02-11 11:46:19 -0500

    RF: modularize GCE settings into namespace class

--- cliquesadmin/gce_utils/__init__.py
+++ cliquesadmin/gce_utils/__init__.py
@@ -8,22 +8,28 @@ from oauth2client.file import Storage
 from oauth2client import tools
 from oauth2client.tools import run_flow
 from googleapiclient.discovery import build
-
 from cliquesadmin import REPOSITORY_PATH
 
-DEFAULT_ZONE = 'us-central1-a'
-API_VERSION = 'v1'
-GCE_URL = 'https://www.googleapis.com/compute/%s/projects/' % (API_VERSION)
-PROJECT_ID = 'mimetic-codex-781'
-CLIENT_SECRETS = os.path.join(REPOSITORY_PATH,'client_secrets.json')
-OAUTH2_STORAGE = os.path.join(REPOSITORY_PATH,'oauth2.dat')
-GCE_SCOPE = 'https://www.googleapis.com/auth/compute'
+class GCESettings:
+    ZONE = 'us-central1-a'
+    API_VERSION = 'v1'
+    GCE_URL = 'https://www.googleapis.com/compute/%s/projects/' % (API_VERSION)
+    GCE_SCOPE = 'https://www.googleapis.com/auth/compute'
+    PROJECT_ID = None
+    CLIENT_SECRETS = None
+    OAUTH2_STORAGE = None
 
-def authenticate_and_build(argv):
+class CliquesGCESettings(GCESettings):
+    PROJECT_ID = 'mimetic-codex-781'
+    CLIENT_SECRETS = os.path.join(REPOSITORY_PATH,'client_secrets.json')
+    OAUTH2_STORAGE = os.path.join(REPOSITORY_PATH,'oauth2.dat')
+
+
+def authenticate_and_build(argv, gce_settings=CliquesGCESettings):
     """
-    Authenticates with OAuth 2.0 credentials, if present, and builds
-    :param argv:
-    :return:
+    Authenticates with OAuth 2.0 credentials, if present, and builds GCE API Service obj
+
+    :return: auth_http, gce_service
     """
     logging.basicConfig(level=logging.INFO)
 
@@ -36,8 +42,8 @@ def authenticate_and_build(argv):
     flags = parser.parse_args(argv[1:])
 
     # Perform OAuth 2.0 authorization.
-    flow = flow_from_clientsecrets(CLIENT_SECRETS, scope=GCE_SCOPE)
-    storage = Storage(OAUTH2_STORAGE)
+    flow = flow_from_clientsecrets(gce_settings.CLIENT_SECRETS, scope=gce_settings.GCE_SCOPE)
+    storage = Storage(gce_settings.OAUTH2_STORAGE)
     credentials = storage.get()
 
     if credentials is None or credentials.invalid:
@@ -46,12 +52,12 @@ def authenticate_and_build(argv):
     auth_http = credentials.authorize(http)
 
     # Build the service
-    gce_service = build('compute', API_VERSION)
+    gce_service = build('compute', gce_settings.API_VERSION)
     # project_url = '%s%s' % (GCE_URL, PROJECT_ID)
 
     return auth_http, gce_service
 
-def _blocking_call(gce_service, auth_http, response):
+def _blocking_call(gce_service, auth_http, response, gce_settings=CliquesGCESettings):
     """Blocks until the operation status is done for the given operation."""
 
     status = response['status']
@@ -62,12 +68,12 @@ def _blocking_call(gce_service, auth_http, response):
     if 'zone' in response:
       zone_name = response['zone'].split('/')[-1]
       request = gce_service.zoneOperations().get(
-          project=PROJECT_ID,
+          project=gce_settings.PROJECT_ID,
           operation=operation_id,
           zone=zone_name)
     else:
       request = gce_service.globalOperations().get(
-           project=PROJECT_ID, operation=operation_id)
+           project=gce_settings.PROJECT_ID, operation=operation_id)
 
     response = request.execute(http=auth_http)
     if response:
