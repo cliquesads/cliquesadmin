b460f3ad97785e989d80ee8ad164dd4fd24a69a7
Ben Liang <bliang@cliquesads.com>
2015-02-13 12:14:49 -0500

    NF: Function to purge old snapshots, added to run script

--- bin/snapshot_all_disks.py
+++ bin/snapshot_all_disks.py
@@ -3,18 +3,20 @@ import logging
 import os
 from datetime import datetime
 from cliquesadmin.gce_utils import authenticate_and_build
-from cliquesadmin.gce_utils.disk_utils import get_all_disks, make_snapshot
+from cliquesadmin.gce_utils.disk_utils import get_all_disks, make_snapshot, purge_old_snapshots
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG,
+                        filename=os.path.expanduser('~/logs/gce_snapshot.log'))
     start = datetime.utcnow()
     logging.info('Starting to create snapshots for all GCE disks at %s' % start)
-    logging.basicConfig(level=logging.DEBUG, filename=os.path.expanduser('~/logs/gce_snapshot.log'))
 
     auth_http, gce_service = authenticate_and_build(sys.argv)
     k = get_all_disks(auth_http, gce_service)
     for d in k:
         resp = make_snapshot(auth_http, gce_service, d)
         logging.info('Snapshot Complete')
+        purge_old_snapshots(auth_http, gce_service,d)
 
     end = datetime.utcnow()
     logging.info('Finished creating snapshots for all GCE disks at %s' % datetime.utcnow())
--- cliquesadmin/gce_utils/disk_utils.py
+++ cliquesadmin/gce_utils/disk_utils.py
@@ -1,5 +1,7 @@
 import sys
 from datetime import datetime
+from time import mktime
+from feedparser import _parse_date as parse_date
 from cliquesadmin.gce_utils import cliques_gce_settings, authenticate_and_build, blocking_call
 
 def get_all_disks(auth_http, gce_service, gce_settings=cliques_gce_settings):
@@ -18,7 +20,7 @@ def get_all_disks(auth_http, gce_service, gce_settings=cliques_gce_settings):
 @blocking_call
 def make_snapshot(auth_http, gce_service, disk_name, gce_settings=cliques_gce_settings):
 
-    snapshot_name = "%s-%s" % (disk_name, datetime.utcnow().strftime("%Y-%m-%d-%H-%M"))
+    snapshot_name = "%s-%s" % (disk_name, datetime.utcnow().strftime("%Y-%m-%d"))
     body = {'sourceDisk': disk_name,
             'name': snapshot_name}
     request = gce_service.disks().createSnapshot(project=gce_settings.PROJECT_ID,
@@ -27,3 +29,38 @@ def make_snapshot(auth_http, gce_service, disk_name, gce_settings=cliques_gce_se
                                                  body=body)
     response = request.execute(http=auth_http)
     return response
+
+def _rfc3339_to_datetime(timestamp):
+    stime = parse_date(timestamp)
+    tfloat = mktime(stime)
+    return datetime.fromtimestamp(tfloat)
+
+@blocking_call
+def delete_snapshot(auth_http, gce_service, snapshot_name, gce_settings=cliques_gce_settings):
+    request = gce_service.snapshots().delete(project=gce_settings.PROJECT_ID,
+                                             snapshot=snapshot_name)
+    response = request.execute(http=auth_http)
+    return response
+
+def purge_old_snapshots(auth_http, gce_service, disk_name, snapshots_to_keep=2, gce_settings=cliques_gce_settings):
+
+    assert snapshots_to_keep >= 1
+
+    snapshot_name_re= "name eq '%s.*'" % disk_name
+    request = gce_service.snapshots().list(project=gce_settings.PROJECT_ID,
+                                           filter=snapshot_name_re)
+    response = request.execute(http=auth_http)
+    snapshots = response['items']
+    #prune older snapshots
+    if len(snapshots) > snapshots_to_keep:
+        key = lambda k: _rfc3339_to_datetime(k['creationTimestamp'])
+        snapshots = sorted(snapshots, key=key, reverse=True)
+        old_snapshots = snapshots[snapshots_to_keep:]
+        for old_snap in old_snapshots:
+            delete_snapshot(auth_http, gce_service, old_snap['name'], gce_settings=gce_settings)
+    return True
+
+if __name__ == "__main__":
+    auth_http, gce_service = authenticate_and_build(sys.argv)
+
+    purge_old_snapshots(auth_http, gce_service, 'mysql-server-a1')
