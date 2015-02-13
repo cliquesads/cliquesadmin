5e3ce40fbc5f0763e2a9805ad6f70105a3626d0a
Ben Liang <bliang@cliquesads.com>
2015-02-13 01:01:39 -0500

    BF: Changed to pull_and_build to fix bash silliness

--- pull_and_build_env.sh
+++ pull_and_build_env.sh
@@ -4,11 +4,11 @@
 git pull
 
 #build virtualenv if it doesn't exist already
-if [[ ! -d $HOME"/repositories/cliquesadmin/venv/" ]]; then
+if [ ! -d $HOME"/repositories/cliquesadmin/venv/" ]; then
     virtualenv venv
 fi
 
-source $HOME/repositories/cliquesadmin/activate
+. $HOME/repositories/cliquesadmin/activate
 
 #install all latest requirements
 pip install -r requirements.txt
