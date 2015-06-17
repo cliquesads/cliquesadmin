40bf2834fe9252a435343d581484439b50ad57bb
Ben Liang <bliang@cliquesads.com>
2015-06-17 01:32:40 -0400

    NF: Handle timestamp to datetime64 conversion properly

--- cliquesadmin/gce_utils/bigquery.py
+++ cliquesadmin/gce_utils/bigquery.py
@@ -21,31 +21,46 @@ jinja_bq_env = Environment(loader=PackageLoader('cliquesadmin', 'bigquery'))
 
 # BigQuery Datatype reference:
 # https://cloud.google.com/bigquery/preparing-data-for-bigquery#datatypes
-# Not sure how to handle TIMESTAMP yet, might want to handle as int if
-# it aways renders as a Unix timestamp
 BQ_NP_TYPE_MAPPING = {
     'STRING': np.dtype(object),
     'INTEGER': np.dtype(int),
     'FLOAT': np.dtype(float),
     'BOOLEAN': np.dtype(bool),
-    'TIMESTAMP': np.dtype(object)
+    'TIMESTAMP': np.dtype(float),
 }
 
 
 def query_response_to_dataframe(query_response):
     """
     Loads query response to pandas dataframe
-    :param query_response: query response from
-    :return:
+
+    Automatically handles type conversions as well, including
+    timestamp to datetime64
+
+    :param query_response: query response from BigQuery API
+    :return: pandas DataFrame
     """
     rows = [tuple(map(lambda field: field['v'], row['f']))
             for row in query_response['rows']]
 
-    dtypes = [(str(field['name']), BQ_NP_TYPE_MAPPING[field['type']])
-              for field in query_response['schema']['fields']]
+    # Now create dtype list of tuples for np.array
+    dtypes = []
+    # save timestamp columns to convert in DF to datetime64 once complete
+    tstamp_cols = []
+    for field in query_response['schema']['fields']:
+        dtype_tuple = (str(field['name']),
+                       BQ_NP_TYPE_MAPPING[field['type']])
+        dtypes.append(dtype_tuple)
+        if field['type'] == 'TIMESTAMP':
+            tstamp_cols.append(field['name'])
 
     arr = np.array(rows, dtype=dtypes)
     df = pd.DataFrame(arr)
+
+    # now go back over dataframe and convert temporary float columns
+    # into datetime64's.
+    for col in tstamp_cols:
+        df[col] = df[col].astype('datetime64[s]')
     return df
 
 
