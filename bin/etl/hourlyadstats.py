209f6edaf9fa7b44b81245fd211d2359c06de1bf
Ben Liang <bliang@cliquesads.com>
2015-06-22 15:40:45 -0400

    NF: Added pager duty API to requirements and added intermediates to ETL script

--- bin/etl/hourlyadstats.py
+++ bin/etl/hourlyadstats.py
@@ -15,29 +15,41 @@ mongo_source_db = config.get('ETL', 'mongodb', 'db')
 
 client = MongoClient(mongo_host, mongo_port)
 client.exchange.authenticate(mongo_user, mongo_pwd, source=mongo_source_db)
-collection = client.exchange.test
-
-bigquery_template = 'hourlyadstats.sql'
-name = 'HourlyAdStats'
 
 if __name__ == '__main__':
-
-    # tmp
-    from datetime import datetime
-    opts = {'destinationTable':{'datasetId': 'ad_events','projectId': cliques_bq_settings.PROJECT_ID,'tableId': 'imp_matched_actions'},
-            'createDisposition': 'CREATE_AS_NEEDED',
-            'writeDisposition': 'WRITE_APPEND'}
-    etl = BigQueryIntermediateETL('imp_matched_actions.sql', cliques_bq_settings, query_options=opts)
-    result = etl.run(start=datetime(2015, 6, 20, 21, 0, 0), end=datetime(2015, 6, 20, 22, 0, 0), lookback=30)
-
+    name = 'HourlyAdStats'
     args = parse_hourly_etl_args(name)
-    etl = BigQueryMongoETL(bigquery_template, cliques_bq_settings, collection)
-    logger.info('Beginning %s ETL for interval %s to %s' % (name, args.start, args.end))
+    logger.info('Beginning %s ETLs for interval %s to %s' % (name, args.start, args.end))
 
+    # IMP_MATCHED_ACTIONS ETL
+    query_opts = {
+        'destinationTable':
+            {
+                'datasetId': 'ad_events',
+                'projectId': cliques_bq_settings.PROJECT_ID,
+                'tableId': 'imp_matched_actions'
+            },
+            'createDisposition': 'CREATE_AS_NEEDED',
+            'writeDisposition': 'WRITE_APPEND'
+    }
+    VIEW_ACTION_LOOKBACK = 30
+    imp_matched_actions_etl = BigQueryIntermediateETL('imp_matched_actions.sql',
+                                                      cliques_bq_settings,
+                                                      query_options=query_opts)
+    logger.info('Now matching imps to actions, storing in BigQuery')
+    imp_matched_result = imp_matched_actions_etl.run(start=args.start,
+                                                     end=args.end,
+                                                     lookback=VIEW_ACTION_LOOKBACK)
+    logger.info('Done')
+
+    # LOAD AGGREGATES TO MONGODB
+    HOURLY_ADSTAT_COLLECTION = client.exchange.test
+    etl = BigQueryMongoETL('hourlyadstats.sql', cliques_bq_settings, HOURLY_ADSTAT_COLLECTION)
+    logger.info('Now loading aggregates to MongoDB')
     result = etl.run(start=args.start, end=args.end)
     if result is not None:
         logger.info('Inserted %s documents into collection %s' %
-                    (len(result.inserted_ids), collection.full_name))
+                    (len(result.inserted_ids), HOURLY_ADSTAT_COLLECTION.full_name))
         logger.info('%s ETL Complete' % name)
     else:
         logger.info('No rows to insert, ETL complete.')
