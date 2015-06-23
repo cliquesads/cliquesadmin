34670c744f882d972241f7f8d874d1d29cc30ca8
Ben Liang <bliang@cliquesads.com>
2015-06-22 23:54:12 -0400

    NF: Added in view & click conversions query to hourlyadstats aggregation

--- cliquesadmin/bigquery/hourlyadstats.sql
+++ cliquesadmin/bigquery/hourlyadstats.sql
@@ -10,10 +10,13 @@ SELECT
   imps.creative AS creative,
   auctions.pub_clique AS pub_clique,
   imps.adv_clique AS adv_clique,
+  actions.actionbeacon as actionbeacon,
   COUNT(auctions.impid) AS impressions,
   COUNT(DISTINCT(auctions.uuid)) AS uniques,
   SUM(auctions.clearprice)/1000 AS spend,
-  COUNT(clicks.clickid) AS clicks
+  COUNT(clicks.clickid) AS clicks,
+  SUM(actions.view_conv) AS view_conv,
+  SUM(actions.click_conv) AS click_conv,
 FROM
   [ad_events.auctions] AS auctions
 INNER JOIN EACH [ad_events.impressions] AS imps
@@ -22,6 +25,25 @@ ON
 LEFT JOIN EACH [ad_events.clicks] AS clicks
 ON
   auctions.impid = clicks.impid
+OUTER JOIN EACH (
+  SELECT
+    IF(c.click_tstamp IS NULL OR c.click_tstamp <= i.imp_tstamp, 1, 0) AS view_conv,
+    IF(c.click_tstamp IS NOT NULL AND c.click_tstamp > i.imp_tstamp, 1, 0) AS click_conv,
+    i.actionid AS actionid,
+    i.impid AS impid,
+    c.clickid AS clickid,
+    i.actionbeacon AS actionbeacon,
+    i.value AS value,
+  FROM
+    [ad_events.imp_matched_actions] AS i
+  OUTER JOIN EACH [ad_events.click_matched_actions] AS c
+  ON
+    c.actionid = i.actionid
+  WHERE
+    i.action_tstamp >= TIMESTAMP('{{ start }}')
+    AND i.action_tstamp < TIMESTAMP('{{ end }}')) AS actions
+ON
+  auctions.impid = actions.impid
 WHERE
   auctions.tstamp >= TIMESTAMP('{{ start }}')
   AND auctions.tstamp < TIMESTAMP('{{ end }}')
@@ -35,4 +57,5 @@ GROUP EACH BY
   creativegroup,
   creative,
   pub_clique,
-  adv_clique
+  adv_clique,
+  actionbeacon
