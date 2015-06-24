bdacee906ef42e45b171b6ade61e8aa4c24d573d
bliang <bliang@cliquesads.com>
2015-06-24 21:18:31 +0000

    modified setup script

--- setup-debian.sh
+++ setup-debian.sh
@@ -4,6 +4,9 @@
 sudo apt-get update
 sudo apt-get install git-core
 sudo apt-get install python-pip
+sudo apt-get install openssl
+sudo apt-get install python-dev
+sudo apt-get install libffi-dev
 
 # now install virtualenv
 sudo pip install virtualenv
