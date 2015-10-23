97a2ded0c8b0e9713b73855b0c8e62a9959bda3b
Ben Liang <bliang@cliquesads.com>
2015-10-23 02:37:53 +0200

    BF: Catch error traceback in logs

--- bin/etl/hourlyadstats.py
+++ bin/etl/hourlyadstats.py
@@ -28,7 +28,6 @@ GLOBAL_QUERY_OPTS = {
         'createDisposition': 'CREATE_AS_NEEDED',
         'writeDisposition': 'WRITE_APPEND'
 }
-
 name = 'HourlyAdStats'
 
 if __name__ == '__main__':
@@ -37,70 +36,75 @@ if __name__ == '__main__':
 
     logger.info('Beginning %s ETLs for interval %s to %s' % (name, args.start, args.end))
 
-    ###########################
-    # IMP_MATCHED_ACTIONS ETL #
-    ###########################
-    imp_matched_query_opts = GLOBAL_QUERY_OPTS
-    imp_matched_query_opts['destinationTable']['tableId'] = 'imp_matched_actions'
-    imp_matched_actions_etl = BigQueryIntermediateETL('imp_matched_actions.sql',
-                                                      cliques_bq_settings,
-                                                      query_options=imp_matched_query_opts)
-    logger.info('Now matching imps to actions, storing in BigQuery')
-    imp_matched_result = imp_matched_actions_etl.run(start=args.start,
-                                                     end=args.end,
-                                                     lookback=view_lookback)
-    logger.info('Done')
+    # Wrap whole thing in blanket exception handler to write to log
+    try:
+        ###########################
+        # IMP_MATCHED_ACTIONS ETL #
+        ###########################
+        imp_matched_query_opts = GLOBAL_QUERY_OPTS
+        imp_matched_query_opts['destinationTable']['tableId'] = 'imp_matched_actions'
+        imp_matched_actions_etl = BigQueryIntermediateETL('imp_matched_actions.sql',
+                                                          cliques_bq_settings,
+                                                          query_options=imp_matched_query_opts)
+        logger.info('Now matching imps to actions, storing in BigQuery')
+        imp_matched_result = imp_matched_actions_etl.run(start=args.start,
+                                                         end=args.end,
+                                                         lookback=view_lookback)
+        logger.info('Done')
 
-    #############################
-    # CLICK_MATCHED_ACTIONS ETL #
-    #############################
-    click_matched_query_opts = GLOBAL_QUERY_OPTS
-    click_matched_query_opts['destinationTable']['tableId'] = 'click_matched_actions'
-    imp_matched_actions_etl = BigQueryIntermediateETL('click_matched_actions.sql',
-                                                      cliques_bq_settings,
-                                                      query_options=click_matched_query_opts)
-    logger.info('Now matching clicks to actions, storing in BigQuery')
-    imp_matched_result = imp_matched_actions_etl.run(start=args.start,
-                                                     end=args.end,
-                                                     lookback=click_lookback)
-    logger.info('Done')
+        #############################
+        # CLICK_MATCHED_ACTIONS ETL #
+        #############################
+        click_matched_query_opts = GLOBAL_QUERY_OPTS
+        click_matched_query_opts['destinationTable']['tableId'] = 'click_matched_actions'
+        imp_matched_actions_etl = BigQueryIntermediateETL('click_matched_actions.sql',
+                                                          cliques_bq_settings,
+                                                          query_options=click_matched_query_opts)
+        logger.info('Now matching clicks to actions, storing in BigQuery')
+        imp_matched_result = imp_matched_actions_etl.run(start=args.start,
+                                                         end=args.end,
+                                                         lookback=click_lookback)
+        logger.info('Done')
 
-    #####################
-    # AUCTION_STATS ETL #
-    #####################
-    auction_query_opts = GLOBAL_QUERY_OPTS
-    auction_query_opts['destinationTable']['tableId'] = 'auction_stats'
-    auction_stats_etl = BigQueryIntermediateETL('auction_stats.sql',
-                                                cliques_bq_settings,
-                                                query_options=auction_query_opts)
+        #####################
+        # AUCTION_STATS ETL #
+        #####################
+        auction_query_opts = GLOBAL_QUERY_OPTS
+        auction_query_opts['destinationTable']['tableId'] = 'auction_stats'
+        auction_stats_etl = BigQueryIntermediateETL('auction_stats.sql',
+                                                    cliques_bq_settings,
+                                                    query_options=auction_query_opts)
 
-    logger.info('Now creating auction stats, containing bid density & clearprice')
-    auction_stats_result = auction_stats_etl.run(start=args.start,
-                                                 end=args.end)
-    logger.info('Done')
+        logger.info('Now creating auction stats, containing bid density & clearprice')
+        auction_stats_result = auction_stats_etl.run(start=args.start,
+                                                     end=args.end)
+        logger.info('Done')
 
-    #################################
-    # AUCTION_STATS DEFAULT ADS ETL #
-    #################################
-    auction_stats_defaults_etl = BigQueryIntermediateETL('auction_stats_defaults.sql',
-                                                         cliques_bq_settings,
-                                                         query_options=auction_query_opts)
+        #################################
+        # AUCTION_STATS DEFAULT ADS ETL #
+        #################################
+        auction_stats_defaults_etl = BigQueryIntermediateETL('auction_stats_defaults.sql',
+                                                             cliques_bq_settings,
+                                                             query_options=auction_query_opts)
 
-    logger.info('Now creating auction stats for all errored auctions (i.e. auctions w/ no bids)')
-    auction_stats_defaults_result = auction_stats_defaults_etl.run(start=args.start,
-                                                                   end=args.end)
-    logger.info('Done')
+        logger.info('Now creating auction stats for all errored auctions (i.e. auctions w/ no bids)')
+        auction_stats_defaults_result = auction_stats_defaults_etl.run(start=args.start,
+                                                                       end=args.end)
+        logger.info('Done')
 
-    ##############################
-    # LOAD AGGREGATES TO MONGODB #
-    ##############################
-    HOURLY_ADSTAT_COLLECTION = client.exchange.hourlyadstats
-    etl = BigQueryMongoETL('hourlyadstats.sql', cliques_bq_settings, HOURLY_ADSTAT_COLLECTION)
-    logger.info('Now loading aggregates to MongoDB')
-    result = etl.run(start=args.start, end=args.end)
-    if result is not None:
-        logger.info('Inserted %s documents into collection %s' %
-                    (len(result.inserted_ids), HOURLY_ADSTAT_COLLECTION.full_name))
-        logger.info('%s ETL Complete' % name)
-    else:
-        logger.info('No rows to insert, ETL complete.')
+        ##############################
+        # LOAD AGGREGATES TO MONGODB #
+        ##############################
+        HOURLY_ADSTAT_COLLECTION = client.exchange.hourlyadstats
+        etl = BigQueryMongoETL('hourlyadstats.sql', cliques_bq_settings, HOURLY_ADSTAT_COLLECTION)
+        logger.info('Now loading aggregates to MongoDB')
+        result = etl.run(start=args.start, end=args.end)
+        if result is not None:
+            logger.info('Inserted %s documents into collection %s' %
+                        (len(result.inserted_ids), HOURLY_ADSTAT_COLLECTION.full_name))
+            logger.info('%s ETL Complete' % name)
+        else:
+            logger.info('No rows to insert, ETL complete.')
+    except:
+        logger.exception('Uncaught exception while running ETL!')
+        raise
