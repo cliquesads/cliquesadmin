865ddde872e80a0a997db25137b1a75c7c0eb6bb
Ben Liang <bliang@cliquesads.com>
2015-12-23 13:47:01 -0500

    NF: Fuck it just hardcode custom ranges, only should be used ad-hoc anyway

--- bin/etl/auctionstats_catchup.py
+++ bin/etl/auctionstats_catchup.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
-
+from datetime import datetime, timedelta
 from cliquesadmin import logger
 from cliquesadmin.pagerduty_utils import stacktrace_to_pd_event, create_pd_event_wrapper
 from cliquesadmin.misc_utils import parse_hourly_etl_args
@@ -35,62 +35,69 @@ name = 'AuctionStats Catchup'
 
 if __name__ == '__main__':
 
-    args = parse_hourly_etl_args(name)
+    # args = parse_hourly_etl_args(name)
+    datetimes = [datetime(2015, 12, 22, h) for h in range(10, 15)]
+    datetimes += [datetime(2015, 12, 22, h) for h in range(16, 24)]
+    datetimes += [datetime(2015, 12, 23, h) for h in range(18)]
 
-    logger.info('Beginning %s ETLs for interval %s to %s' % (name, args.start, args.end))
+    for dt in datetimes:
+        start = dt
+        end = dt + timedelta(hours=1)
 
-    # Wrap whole thing in blanket exception handler to write to log
-    try:
-        #####################
-        # AUCTION_STATS ETL #
-        #####################
-        auction_query_opts = GLOBAL_QUERY_OPTS
-        auction_query_opts['destinationTable']['tableId'] = 'auction_stats'
-        auction_stats_etl = BigQueryIntermediateETL('auction_stats.sql',
-                                                    cliques_bq_settings,
-                                                    query_options=auction_query_opts)
+        logger.info('Beginning %s ETLs for interval %s to %s' % (name, start, end))
 
-        logger.info('Now creating auction stats, containing bid density & clearprice')
-        auction_stats_result = auction_stats_etl.run(start=args.start,
-                                                     end=args.end,
-                                                     error_callback=pd_error_callback)
-        logger.info('Done')
+        # Wrap whole thing in blanket exception handler to write to log
+        try:
+            #####################
+            # AUCTION_STATS ETL #
+            #####################
+            auction_query_opts = GLOBAL_QUERY_OPTS
+            auction_query_opts['destinationTable']['tableId'] = 'auction_stats'
+            auction_stats_etl = BigQueryIntermediateETL('auction_stats.sql',
+                                                        cliques_bq_settings,
+                                                        query_options=auction_query_opts)
 
-        #################################
-        # AUCTION_STATS DEFAULT ADS ETL #
-        #################################
-        # auction_stats_defaults_etl = BigQueryIntermediateETL('auction_stats_defaults.sql',
-        #                                                      cliques_bq_settings,
-        #                                                      query_options=auction_query_opts)
-        #
-        # logger.info('Now creating auction stats for all errored auctions (i.e. auctions w/ no bids)')
-        # auction_stats_defaults_result = auction_stats_defaults_etl.run(start=args.start,
-        #                                                                end=args.end,
-        #                                                                error_callback=pd_error_callback)
-        # logger.info('Done')
+            logger.info('Now creating auction stats, containing bid density & clearprice')
+            auction_stats_result = auction_stats_etl.run(start=start,
+                                                         end=end,
+                                                         error_callback=pd_error_callback)
+            logger.info('Done')
 
-        ########################################
-        # DELETE INVALID AGGREGATES TO MONGODB #
-        ########################################
-        logger.info('Now deleting invalid aggregate results for this hour in MongoDB HourlyAdStats')
-        HOURLY_ADSTAT_COLLECTION = client.exchange.hourlyadstats
-        result = HOURLY_ADSTAT_COLLECTION.delete_many({'hour': args.start})
-        logger.info('Successfully deleted %s rows' % result.deleted_count)
+            #################################
+            # AUCTION_STATS DEFAULT ADS ETL #
+            #################################
+            # auction_stats_defaults_etl = BigQueryIntermediateETL('auction_stats_defaults.sql',
+            #                                                      cliques_bq_settings,
+            #                                                      query_options=auction_query_opts)
+            #
+            # logger.info('Now creating auction stats for all errored auctions (i.e. auctions w/ no bids)')
+            # auction_stats_defaults_result = auction_stats_defaults_etl.run(start=args.start,
+            #                                                                end=args.end,
+            #                                                                error_callback=pd_error_callback)
+            # logger.info('Done')
 
-        ##############################
-        # LOAD AGGREGATES TO MONGODB #
-        ##############################
-        etl = BigQueryMongoETL('hourlyadstats.sql', cliques_bq_settings, HOURLY_ADSTAT_COLLECTION)
-        logger.info('Now loading aggregates to MongoDB')
-        result = etl.run(start=args.start, end=args.end, error_callback=pd_error_callback)
-        if result is not None:
-            logger.info('Inserted %s documents into collection %s' %
-                        (len(result.inserted_ids), HOURLY_ADSTAT_COLLECTION.full_name))
-            logger.info('%s ETL Complete' % name)
-        else:
-            logger.info('No rows to insert, ETL complete.')
-    except:
-        # Trigger incident in PagerDuty, then write out to log file
-        stacktrace_to_pd_event(pd_subdomain, pd_api_key, pd_service_key)
-        logger.exception('Uncaught exception while running ETL!')
-        raise
+            ########################################
+            # DELETE INVALID AGGREGATES TO MONGODB #
+            ########################################
+            logger.info('Now deleting invalid aggregate results for this hour in MongoDB HourlyAdStats')
+            HOURLY_ADSTAT_COLLECTION = client.exchange.hourlyadstats
+            result = HOURLY_ADSTAT_COLLECTION.delete_many({'hour': start})
+            logger.info('Successfully deleted %s rows' % result.deleted_count)
+
+            ##############################
+            # LOAD AGGREGATES TO MONGODB #
+            ##############################
+            etl = BigQueryMongoETL('hourlyadstats.sql', cliques_bq_settings, HOURLY_ADSTAT_COLLECTION)
+            logger.info('Now loading aggregates to MongoDB')
+            result = etl.run(start=start, end=end, error_callback=pd_error_callback)
+            if result is not None:
+                logger.info('Inserted %s documents into collection %s' %
+                            (len(result.inserted_ids), HOURLY_ADSTAT_COLLECTION.full_name))
+                logger.info('%s ETL Complete' % name)
+            else:
+                logger.info('No rows to insert, ETL complete.')
+        except:
+            # Trigger incident in PagerDuty, then write out to log file
+            stacktrace_to_pd_event(pd_subdomain, pd_api_key, pd_service_key)
+            logger.exception('Uncaught exception while running ETL!')
+            raise
