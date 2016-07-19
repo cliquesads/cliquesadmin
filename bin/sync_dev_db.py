b6e7bd8241ec0fbcbb2e6217abf287b9363ba4a2
Ben Liang <bliang@cliquesads.com>
2016-07-19 17:54:19 -0500

    BF

--- bin/sync_dev_db.py
+++ bin/sync_dev_db.py
@@ -11,7 +11,7 @@ SYNC = [
     'organizations',
     'users',
     'payments',
-    'insertionorders'
+    'insertionorders',
     'accesscodes',
     'termsandconditions',
     'demohourlyadstats',
