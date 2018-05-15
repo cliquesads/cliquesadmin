65c3256c06725cf8276db5f71262fba3e3d0ea89
Ben Liang <bliang@cliquesads.com>
2018-05-15 16:33:46 -0400

    NF: Split ETL SQL files into CPC & CPM specific queries

---
+++ cliquesadmin/bigquery/geoadstats/geoadstats_imps_clicks_cpc.sql
@@ -0,0 +1,49 @@
+SELECT
+  TIMESTAMP('{{ start }}') AS hour,
+  auctions.publisher AS publisher,
+  auctions.site AS site,
+  auctions.page AS page,
+  imps.advertiser AS advertiser,
+  imps.campaign AS campaign,
+  auctions.pub_clique AS pub_clique,
+  imps.adv_clique AS adv_clique,
+  auctions.country AS country,
+  auctions.country + '-' + auctions.region AS region,
+  auctions.city AS city,
+  auctions.metro AS DMA,
+  auctions.zip AS zip,
+  SUM(auction_stats.num_bids) AS bids,
+  SUM(auction_stats.clearprice * IF(clicks.clickid is null, 0, 1)) AS spend,
+  COUNT(auctions.impid) AS imps,
+  COUNT(DISTINCT(auctions.uuid)) AS uniques,
+  COUNT(clicks.clickid) AS clicks,
+  0 AS view_convs,
+  0 AS click_convs
+FROM
+  [{{ dataset }}.auctions] AS auctions
+INNER JOIN EACH [{{ dataset }}.impressions] AS imps
+ON
+  auctions.impid = imps.impid
+INNER JOIN EACH [{{ dataset }}.auction_stats] AS auction_stats
+ON
+  auctions.impid = auction_stats.impid AND
+  auctions.auctionId = auction_stats.auctionId
+LEFT JOIN EACH [{{ dataset }}.clicks] AS clicks
+ON
+  auctions.impid = clicks.impid
+WHERE
+  auctions.tstamp >= TIMESTAMP('{{ start }}')
+  AND auctions.tstamp < TIMESTAMP('{{ end }}')
+GROUP EACH BY
+  publisher,
+  site,
+  page,
+  advertiser,
+  campaign,
+  pub_clique,
+  adv_clique,
+  country,
+  region,
+  city,
+  DMA,
+  zip
---
+++ cliquesadmin/bigquery/keywordadstats/keywordadstats_imps_clicks_cpc.sql
@@ -0,0 +1,47 @@
+SELECT
+	TIMESTAMP('{{ start }}') AS hour,
+	auctions.publisher AS publisher,
+	auctions.site AS site,
+	auctions.page AS page,
+	auctions.placement AS placement,
+	auctions.keywords AS keywords,
+	imps.advertiser AS advertiser,
+	imps.campaign AS campaign,
+	imps.creativegroup AS creativegroup,
+	imps.creative AS creative,
+	auctions.pub_clique AS pub_clique,
+	imps.adv_clique AS adv_clique,
+	SUM(auction_stats.num_bids) AS bids,
+	SUM(auction_stats.clearprice * IF(clicks.clickid is null, 0, 1)) AS spend,
+	COUNT(auctions.impid) AS imps,
+	COUNT(DISTINCT(auctions.uuid)) AS uniques,
+	COUNT(clicks.clickid) AS clicks,
+	0 AS view_convs,
+	0 AS click_convs,
+FROM
+	[{{ dataset }}.auctions] AS auctions
+INNER JOIN EACH [{{ dataset }}.impressions] AS imps
+ON
+	auctions.impid = imps.impid
+INNER JOIN EACH [{{ dataset }}.auction_stats] AS auction_stats
+ON
+	auctions.impid = auction_stats.impid AND
+	auctions.auctionId = auction_stats.auctionId
+LEFT JOIN EACH [{{ dataset }}.clicks] AS clicks
+ON
+	auctions.impid = clicks.impid
+WHERE
+	auctions.tstamp >= TIMESTAMP('{{ start }}')
+	AND auctions.tstamp < TIMESTAMP('{{ end }}')
+GROUP EACH BY
+	publisher,
+	site,
+	page,
+	placement,
+	advertiser,
+	campaign,
+	creativegroup,
+	creative,
+	pub_clique,
+	adv_clique,
+	keywords
